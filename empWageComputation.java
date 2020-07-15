371a0a088e3645f6589a97a715bd7fefaf963965
sujit gargate <sujitgargate5893@gmail.com>
2020-07-15 16:55:09 +0530

    calculating Employee 20 days Wage

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,35 +1,45 @@
 
-//This is EmpSwitchCase
+// This is Emp20DayWage
 
-public class EmpSwitchCase{
+public class Emp20DayWage{
 
 	 //Constants
-         public static final int IS_FULL_TIME=1;
-         public static final int IS_PART_TIME=2;
-         public static final int EMP_RATE_PER_HOUR=20;
-
+        public static final int IS_FULL_TIME=1;
+        public static final int IS_PART_TIME=2;
+        public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
+	
 	public static void main(String args[]){
+		
 		//Variables
 		int empHrs=0;
 		int empWage=0;
-		//COMPUTATION
-		double empCheck=Math.floor(Math.random()*10)%3;
-		switch ((int)empCheck){
+		int totalEmpWage=0;
+		for(int day=0;day<NUM_OF_WORKING_DAYS;day++){
 		
-		case IS_FULL_TIME:
-			empHrs=8;
-			break;
+		
+			//COMPUTATION
+			double empCheck=Math.floor(Math.random()*10)%3;
+			switch ((int)empCheck){
 
-		case IS_PART_TIME:
-                        empHrs=4;
-			 break;
+			case IS_FULL_TIME:
+				empHrs=8;
+				break;
 
-		default:
-			empHrs=0;
+			case IS_PART_TIME:
+	                        empHrs=4;
+				 break;
+
+			default:
+				empHrs=0;
+			}
+			empWage=EMP_RATE_PER_HOUR*empHrs;
+			totalEmpWage+=empWage;
+			System.out.println(" Employee Wage "+empWage);
 		}
-		empWage=EMP_RATE_PER_HOUR*empHrs;
-		System.out.println(" Employee Wage "+empWage);
+			System.out.println(" Total Employee Wage "+totalEmpWage);
 	}
 }
 
-//This is end of EmpSwitchCase
+
+// This is end Emp20DayWage
