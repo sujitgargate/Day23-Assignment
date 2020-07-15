fc8c67e5fec7b9e5a05497db75a8ad37f29d0b41
sujit gargate <sujitgargate5893@gmail.com>
2020-07-15 16:48:55 +0530

    Employee part time full time added

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,11 +1,30 @@
-//This is Hello Message Printing 
+//This is Part_Full time  //
 
-public class HelloMessage{
+public class EmpPartFullTime{
 
 	public static void main(String args[]){
-		System.out.println("Hello message");
+		//Constants
+		int IS_FULL_TIME=1;
+		int IS_PART_TIME=2;
+		int EMP_RATE_PER_HOUR=20;
+		//Variables
+		int empHrs=0;
+		int empWage=0;
+		//COMPUTATION
+		double empCheck=Math.floor(Math.random()*10)%3;
+		if(empCheck==IS_FULL_TIME){
+			empHrs=8;
+
+		}else if(empCheck==IS_PART_TIME){
+                        empHrs=4;
+                }
+
+		else{
+			empHrs=0;
+		}
+		empWage=EMP_RATE_PER_HOUR*empHrs;
+		System.out.println(" Employee Wage "+empWage);
 	}
 }
 
-//End of Hello Message//
-
+//This is end of Part_Full time  //
