af7361d53fd4c8c8777ec01d11597b270bb70eb0
sujit gargate <sujitgargate5893@gmail.com>
2020-07-15 16:35:46 +0530

    Hello Welcome message

---
+++ empWageComputation.java
@@ -0,0 +1,11 @@
+//This is Hello Message Printing 
+
+public class HelloMessage{
+
+	public static void main(String args[]){
+		System.out.println("Hello message");
+	}
+}
+
+//End of Hello Message//
+
