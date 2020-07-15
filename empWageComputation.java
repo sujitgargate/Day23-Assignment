13b54f149531c085d3cf2965c4a585bb9febbd84
sujit gargate <sujitgargate5893@gmail.com>
2020-07-15 16:58:25 +0530

    Adding limit to 100 Working Hours and 20 Working

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,45 +1,43 @@
 
-// This is Emp20DayWage
+//This is Employee 100 hours and 20 days limit
 
-public class Emp20DayWage{
-
-	 //Constants
-        public static final int IS_FULL_TIME=1;
-        public static final int IS_PART_TIME=2;
-        public static final int EMP_RATE_PER_HOUR=20;
+public class empTotalWage
+{
+	//Constants
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
-	
-	public static void main(String args[]){
-		
-		//Variables
-		int empHrs=0;
-		int empWage=0;
-		int totalEmpWage=0;
-		for(int day=0;day<NUM_OF_WORKING_DAYS;day++){
-		
-		
-			//COMPUTATION
-			double empCheck=Math.floor(Math.random()*10)%3;
-			switch ((int)empCheck){
+	public static final int MAX_HRS_IN_MONTH=100;
 
+public static void main(String[ ] args)
+{
+	//variables
+	int empHrs = 0;
+	int empWage = 0;
+	int totalEmpWage = 0, totalWorkingDays=0, totalEmpHrs=0;
+	while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+	{
+		totalWorkingDays++;
+		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck)
+		{
 			case IS_FULL_TIME:
-				empHrs=8;
+				empHrs = 8;
 				break;
-
 			case IS_PART_TIME:
-	                        empHrs=4;
-				 break;
-
+				empHrs = 4;
+				break;
 			default:
-				empHrs=0;
-			}
-			empWage=EMP_RATE_PER_HOUR*empHrs;
-			totalEmpWage+=empWage;
-			System.out.println(" Employee Wage "+empWage);
+				empHrs = 0;
 		}
-			System.out.println(" Total Employee Wage "+totalEmpWage);
+		totalEmpHrs += empHrs;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		totalEmpWage += empWage;
+		System.out.println("Employee Wage : "+empWage);
+	}
+	System.out.println("Total Employee Wage: "+totalEmpWage);
 	}
 }
 
-
-// This is end Emp20DayWage
+//This end of limit to 100 hours file 
